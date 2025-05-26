b4c6b73a727bc6745f8003ee4091a225e2edd86e
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2025-05-26 08:21:24 -0300

    feat: new run_sync function implementation

--- rsb/coroutines/run_sync.py
+++ rsb/coroutines/run_sync.py
@@ -31,26 +31,36 @@ def run_sync[**P, _T](
     async def _async_wrapper() -> _T:
         return await func(*args, **kwargs)
 
+    # Try to get the running loop, but handle the case where there isn't one
     try:
         loop = asyncio.get_running_loop()
+        loop_is_running = True
     except RuntimeError:
+        # No running event loop - we're in a synchronous context
+        loop = None
+        loop_is_running = False
+
+    # If there's no running loop, just use asyncio.run
+    if not loop_is_running:
         return asyncio.run(_async_wrapper())
 
+    # We have a running loop - need to handle this carefully
     if threading.current_thread() is threading.main_thread():
-        if not loop.is_running():
-            return loop.run_until_complete(_async_wrapper())
-        else:
+        # We're in the main thread with a running loop
+        # We need to run in a separate thread to avoid blocking
+        def run_in_new_loop():
+            new_loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(new_loop)
+            try:
+                return new_loop.run_until_complete(_async_wrapper())
+            finally:
+                new_loop.close()
 
-            def run_in_new_loop():
-                new_loop = asyncio.new_event_loop()
-                asyncio.set_event_loop(new_loop)
-                try:
-                    return new_loop.run_until_complete(_async_wrapper())
-                finally:
-                    new_loop.close()
-
-            with ThreadPoolExecutor() as pool:
-                future = pool.submit(run_in_new_loop)
-                return future.result(timeout)
+        with ThreadPoolExecutor() as pool:
+            future = pool.submit(run_in_new_loop)
+            return future.result(timeout)
     else:
+        # We're in a background thread with a running loop in the main thread
+        # Use run_coroutine_threadsafe to schedule on the main loop
+        assert loop is not None  # loop_is_running=True guarantees loop is not None
         return asyncio.run_coroutine_threadsafe(_async_wrapper(), loop).result(timeout)
